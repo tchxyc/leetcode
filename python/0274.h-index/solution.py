4ddb4b5f51c6eee957896e39340a93e0fb8c7676
tchxyc <idashatao@gmail.com>
2023-10-29 12:31:55 +0800

    binary_search

---
+++ python/0274.h-index/solution.py
@@ -0,0 +1,49 @@
+# Created by Jones at 2023/10/29 12:28
+# leetgo: dev
+# https://leetcode.cn/problems/h-index/
+
+from typing import *
+from leetgo_py import *
+
+from bisect import bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cache, cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, heapreplace
+from itertools import accumulate, chain, count, pairwise, zip_longest
+from math import ceil, comb, floor, gcd, inf, isqrt, log2, perm, sqrt
+from operator import xor
+from string import ascii_lowercase
+from typing import List, Optional
+
+# @lc code=begin
+# from sortedcontainers import SortedList
+
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+
+        l = 0
+        r = n
+
+        while l <= r:
+            mid = (l + r) >> 1
+
+            def check(mid):
+                return sum(x >= mid for x in citations) >= mid
+
+            if check(mid):
+                l = mid + 1
+            else:
+                r = mid - 1
+        return r
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    citations: List[int] = deserialize("List[int]", read_line())
+    ans = Solution().hIndex(citations)
+
+    print("\noutput:", serialize(ans))
