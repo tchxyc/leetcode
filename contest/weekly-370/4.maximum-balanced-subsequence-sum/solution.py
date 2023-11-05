6c30b83ad2560843fdbb224e00df9e5d7facf50c
tchxyc <idashatao@gmail.com>
2023-11-05 13:40:24 +0800

    BIT/Segment Tree

--- contest/weekly-370/4.maximum-balanced-subsequence-sum/solution.py
+++ contest/weekly-370/4.maximum-balanced-subsequence-sum/solution.py
@@ -23,23 +23,20 @@ from typing import List, Optional
 
 class BIT:
     def __init__(self, n: int) -> None:
-        self.n = n + 1
-        self.q = {}
+        self.q = [0] * (n + 1)
 
     def lowbit(self, x: int):
         return x & -x
 
-    def update(self, x: int, val: int):
-        while x < self.n:
-            if x not in self.q or val > self.q[x]:
-                self.q[x] = val
+    def update(self, x: int, d=1):
+        while x < len(self.q):
+            self.q[x] = max(self.q[x], d)
             x += self.lowbit(x)
 
     def query(self, x: int) -> int:
-        res = -(10**18)
+        res = 0
         while x > 0:
-            if x in self.q and self.q[x] > res:
-                res = self.q[x]
+            res = max(res, self.q[x])
             x -= self.lowbit(x)
         return res
 
@@ -49,18 +46,19 @@ class Solution:
         if max(nums) <= 0:
             return max(nums)
         idx = [x - i for i, x in enumerate(nums)]
-        n = len(nums)
-        dp = [0] * n
-        bit = BIT(n)
-        print(idx)
+
+        q = sorted(set(idx))
+        mp = {v: i for i, v in enumerate(q, 1)}
+        bit = BIT(len(mp))
         for i, x in enumerate(idx):
             if nums[i] <= 0:
                 continue
-            dp[x] = max(dp[x], bit.query(x) + nums[i])
-            bit.update(x, dp[x])
-            print(bit.q)
+            j = mp[x]
+            cur = bit.query(j) + nums[i]
+            bit.update(j, cur)
+            # print(bit.q)
 
-        return bit.query(max(idx))
+        return bit.query(len(mp))
 
 
 # @lc code=end
