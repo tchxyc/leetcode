b156d2680403290ddb50fae28df88a83488db381
tchxyc <idashatao@gmail.com>
2023-11-05 12:07:23 +0800

    unsolved

---
+++ contest/weekly-370/3.maximum-score-after-applying-operations-on-a-tree/solution.py
@@ -0,0 +1,63 @@
+# Created by Jones at 2023/11/05 10:30
+# leetgo: dev
+# https://leetcode.cn/problems/maximum-score-after-applying-operations-on-a-tree/
+# https://leetcode.cn/contest/weekly-contest-370/problems/maximum-score-after-applying-operations-on-a-tree/
+
+from typing import *
+from leetgo_py import *
+
+from bisect import bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cache, cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, heapreplace
+from itertools import accumulate, chain, count, pairwise, zip_longest
+from math import ceil, comb, floor, gcd, inf, isqrt, log2, perm, sqrt
+from operator import xor
+from string import ascii_lowercase
+from typing import List, Optional
+
+# @lc code=begin
+# from sortedcontainers import SortedList
+
+
+class Solution:
+    def maximumScoreAfterOperations(
+        self, edges: List[List[int]], values: List[int]
+    ) -> int:
+        n = len(values)
+
+        g = [[] for _ in range(n)]
+
+        for x, y in edges:
+            g[x].append(y)
+            g[y].append(x)
+
+        @cache
+        def dfs(x, fa):
+            op1 = values[x]
+            op2 = 0
+            for y in g[x]:
+                if y == fa:
+                    continue
+                s1, s2 = dfs(y, x)
+                if min(s1, s2) != 0:
+                    op1 += max(s1, s2)
+                else:
+                    op1 += s2
+                op2 += max(s1, s2)
+            # print(x, op1, op2)
+            return op1, op2
+
+        op1, op2 = dfs(0, -1)
+        return max(op1, op2)
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    edges: List[List[int]] = deserialize("List[List[int]]", read_line())
+    values: List[int] = deserialize("List[int]", read_line())
+    ans = Solution().maximumScoreAfterOperations(edges, values)
+
+    print("\noutput:", serialize(ans))
