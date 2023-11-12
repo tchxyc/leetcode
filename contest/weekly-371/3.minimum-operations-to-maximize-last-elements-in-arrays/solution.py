285495b587d3e9fd95e6bbe8eac35ae6bfe09f23
tchxyc <idashatao@gmail.com>
2023-11-12 21:46:41 +0800

    Greedy

--- contest/weekly-371/3.minimum-operations-to-maximize-last-elements-in-arrays/solution.py
+++ contest/weekly-371/3.minimum-operations-to-maximize-last-elements-in-arrays/solution.py
@@ -25,30 +25,21 @@ class Solution:
     def minOperations(self, nums1: List[int], nums2: List[int]) -> int:
         n = len(nums1)
         a, b = nums1[-1], nums2[-1]
-        res = 0
-        # don't change last
-        for i in range(n):
-            x, y = nums1[i], nums2[i]
-            if x <= a and y <= b:
-                continue
-            if y <= a and x <= b:
-                res += 1
-            else:
-                res = inf
-                break
-        res2 = 1
-        a, b = b, a
-        for i in range(n - 1):
-            x, y = nums1[i], nums2[i]
-            if x <= a and y <= b:
-                continue
-            if y <= a and x <= b:
-                res2 += 1
-            else:
-                res2 = inf
-                break
 
-        res = min(res, res2)
+        def f(a, b):
+            res = 0
+            for i in range(n - 1):
+                x, y = nums1[i], nums2[i]
+                if x <= a and y <= b:
+                    continue
+                if y <= a and x <= b:
+                    res += 1
+                else:
+                    res = inf
+                    break
+            return res
+
+        res = min(f(a, b), 1 + f(b, a))
         if res == inf:
             return -1
         return res
