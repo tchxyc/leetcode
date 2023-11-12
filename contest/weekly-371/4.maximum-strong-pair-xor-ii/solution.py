84d01bebcd0d67e0c9ec1dbb19bd1203301c1f24
tchxyc <idashatao@gmail.com>
2023-11-12 22:03:25 +0800

    binary_trie

--- contest/weekly-371/4.maximum-strong-pair-xor-ii/solution.py
+++ contest/weekly-371/4.maximum-strong-pair-xor-ii/solution.py
@@ -21,39 +21,70 @@ from typing import List, Optional
 # from sortedcontainers import SortedList
 
 
+class Node:
+    __slots__ = "children", "cnt"
+
+    def __init__(self):
+        self.children = [None, None]
+        self.cnt = 0  # 子树大小
+
+
+class Trie:
+    HIGH_BIT = 19
+
+    def __init__(self):
+        self.root = Node()
+
+    # 添加 val
+    def insert(self, val: int) -> None:
+        cur = self.root
+        for i in range(Trie.HIGH_BIT, -1, -1):
+            bit = (val >> i) & 1
+            if cur.children[bit] is None:
+                cur.children[bit] = Node()
+            cur = cur.children[bit]
+            cur.cnt += 1  # 维护子树大小
+        return cur
+
+    # 删除 val，但不删除节点
+    # 要求 val 必须在 trie 中
+    def remove(self, val: int) -> None:
+        cur = self.root
+        for i in range(Trie.HIGH_BIT, -1, -1):
+            cur = cur.children[(val >> i) & 1]
+            cur.cnt -= 1  # 维护子树大小
+        return cur
+
+    # 返回 val 与 trie 中一个元素的最大异或和
+    # 要求 trie 中至少有一个元素
+    def max_xor(self, val: int) -> int:
+        cur = self.root
+        ans = 0
+        for i in range(Trie.HIGH_BIT, -1, -1):
+            bit = (val >> i) & 1
+            # 如果 cur.children[bit^1].cnt == 0，视作空节点
+            if cur.children[bit ^ 1] and cur.children[bit ^ 1].cnt:
+                ans |= 1 << i
+                bit ^= 1
+            cur = cur.children[bit]
+        return ans
+
+
 class Solution:
     def maximumStrongPairXor(self, nums: List[int]) -> int:
         # `|x - y| <= min(x, y)`
-
-        # assume x < y
-        # y - x <= x
-
+        # assume x < y => y - x <= x
         # x < y <= 2x
         nums = sorted(set(nums))
         res = 0
         l = 0
-        for r, y in enumerate(nums):
+        trie = Trie()
+        for _, y in enumerate(nums):
             while nums[l] * 2 < y:
+                trie.remove(nums[l])
                 l += 1
-            # find [l..r] ^ y
-            # y = 101110
-            L = l
-            R = r - 1
-            tmp = 0
-            for i in range(y.bit_length() - 1, -1, -1):
-                if y >> i & 1:
-                    j = bisect_left(nums, 1 << i, lo=L, hi=R) - 1
-                    if j >= L:
-                        R = j
-                        tmp |= 1 << i
-                else:
-                    j = bisect_left(nums, 1 << i, lo=L, hi=R)
-                    if j <= R:
-                        L = j
-                        tmp |= 1 << i
-                if L > R:
-                    break
-            res = max(res, tmp)
+            trie.insert(y)
+            res = max(res, trie.max_xor(y))
         return res
 
 
