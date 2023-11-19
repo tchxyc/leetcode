c0c376570b4dcf9d157f1f0e0d315885fde94d1b
tchxyc <idashatao@gmail.com>
2023-11-19 13:23:36 +0800

    fix

--- python/BIT.py
+++ python/BIT.py
@@ -1,11 +1,17 @@
 class BIT:
-    def __init__(self, n: int) -> None:
+    def __init__(self, nums: list[int]) -> None:
+        n = len(nums)
+        self.nums = [0] * n
         self.q = [0] * (n + 1)
+        for i, x in enumerate(nums, 1):
+            self.update(i, x)
 
     def lowbit(self, x: int):
         return x & -x
 
-    def update(self, x: int, d: int):
+    def update(self, i: int, x: int):
+        d = x - self.nums[i - 1]
+        self.nums[i - 1] = x
         while x < len(self.q):
             self.q[x] += d
             x += self.lowbit(x)
@@ -13,7 +19,7 @@ class BIT:
     def query(self, x: int) -> int:
         res = 0
         while x > 0:
-            res += self.q[x]
+            res = max(res, self.q[x])
             x -= self.lowbit(x)
         return res
 
