634e3292283bd46d83e8ed6251ccab7209966696
tchxyc <idashatao@gmail.com>
2023-12-08 14:15:33 +0800

    update

--- python/BIT.py
+++ python/BIT.py
@@ -1,20 +1,14 @@
 class BIT:
-    def __init__(self, nums: list[int]) -> None:
-        n = len(nums)
-        self.nums = [0] * n
+    def __init__(self, n: int) -> None:
         self.q = [0] * (n + 1)
-        for i, x in enumerate(nums, 1):
-            self.update(i, x)
 
     def lowbit(self, x: int):
         return x & -x
 
     def update(self, i: int, x: int):
-        d = x - self.nums[i - 1]
-        self.nums[i - 1] = x
-        while x < len(self.q):
-            self.q[x] += d
-            x += self.lowbit(x)
+        while i < len(self.q):
+            self.q[i] = max(self.q[i], x)
+            i += self.lowbit(i)
 
     def query(self, x: int) -> int:
         res = 0
@@ -22,6 +16,32 @@ class BIT:
             res = max(res, self.q[x])
             x -= self.lowbit(x)
         return res
+
+
+# class BIT:
+#     def __init__(self, nums: list[int]) -> None:
+#         n = len(nums)
+#         self.nums = [0] * n
+#         self.q = [0] * (n + 1)
+#         for i, x in enumerate(nums, 1):
+#             self.update(i, x)
+
+#     def lowbit(self, x: int):
+#         return x & -x
+
+#     def update(self, i: int, x: int):
+#         d = x - self.nums[i - 1]
+#         self.nums[i - 1] = x
+#         while x < len(self.q):
+#             self.q[x] += d
+#             x += self.lowbit(x)
+
+#     def query(self, x: int) -> int:
+#         res = 0
+#         while x > 0:
+#             res = max(res, self.q[x])
+#             x -= self.lowbit(x)
+#         return res
 
 
 # class BIT:
