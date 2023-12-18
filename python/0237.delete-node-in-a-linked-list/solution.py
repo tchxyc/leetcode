55dac3b7b9979e1c377bc65709e9d5b541a3ac0b
tchxyc <idashatao@gmail.com>
2023-12-18 21:50:30 +0800

    Linked List

---
+++ python/0237.delete-node-in-a-linked-list/solution.py
@@ -0,0 +1,48 @@
+# Created by Jones at 2023/12/18 21:47
+# leetgo: dev
+# https://leetcode.cn/problems/delete-node-in-a-linked-list/
+
+from typing import *
+from leetgo_py import *
+
+from bisect import bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cache, cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, heapreplace
+from itertools import accumulate, chain, count, pairwise, zip_longest
+from math import ceil, comb, floor, gcd, inf, isqrt, log2, perm, sqrt
+from operator import xor
+from string import ascii_lowercase
+from typing import List, Optional
+
+# @lc code=begin
+# from sortedcontainers import SortedList
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+
+
+# @lc code=end
+
+# Warning: this is a manual question, the generated test code may be incorrect.
+if __name__ == "__main__":
+    head: ListNode = deserialize("ListNode", read_line())
+    node: int = deserialize("int", read_line())
+    deleteNode(head, node)
+    ans = head
+
+    print("\noutput:", serialize(ans))
