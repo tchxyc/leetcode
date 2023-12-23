0346c2d499a841d87b9f0890f71172a69aa78fa0
tchxyc <idashatao@gmail.com>
2023-12-23 23:53:08 +0800

    Two Pointer

---
+++ contest/biweekly-120/3.count-the-number-of-incremovable-subarrays-ii/solution.py
@@ -0,0 +1,54 @@
+# Created by Jones at 2023/12/23 22:30
+# leetgo: dev
+# https://leetcode.cn/problems/count-the-number-of-incremovable-subarrays-ii/
+# https://leetcode.cn/contest/biweekly-contest-120/problems/count-the-number-of-incremovable-subarrays-ii/
+
+from typing import *
+from leetgo_py import *
+
+from bisect import bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cache, cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, heapreplace
+from itertools import accumulate, chain, count, pairwise, zip_longest
+from math import ceil, comb, floor, gcd, inf, isqrt, log2, perm, sqrt
+from operator import xor
+from string import ascii_lowercase
+from typing import List, Optional
+
+# @lc code=begin
+# from sortedcontainers import SortedList
+
+
+class Solution:
+    def incremovableSubarrayCount(self, nums: List[int]) -> int:
+        n = len(nums)
+
+        right = n - 2
+
+        while right >= 0 and nums[right] < nums[right + 1]:
+            right -= 1
+        if right == -1:
+            return n * (n + 1) // 2
+        res = n - right
+        right += 1
+
+        for left in range(n):
+            # print(left, res)
+            if left > 0 and nums[left] <= nums[left - 1]:
+                break
+            while right < n and nums[right] <= nums[left]:
+                right += 1
+
+            res += n - right + 1
+        return res
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    nums: List[int] = deserialize("List[int]", read_line())
+    ans = Solution().incremovableSubarrayCount(nums)
+
+    print("\noutput:", serialize(ans))
