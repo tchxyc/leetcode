b1bc7625ef2b1131ac6e9e54c936e4ae984b2804
tchxyc <idashatao@gmail.com>
2024-01-06 23:18:34 +0800

    Bit Manipulation

---
+++ contest/biweekly-121/2.minimum-number-of-operations-to-make-array-xor-equal-to-k/solution.py
@@ -0,0 +1,46 @@
+# Created by Jones at 2024/01/06 22:30
+# leetgo: dev
+# https://leetcode.cn/problems/minimum-number-of-operations-to-make-array-xor-equal-to-k/
+# https://leetcode.cn/contest/biweekly-contest-121/problems/minimum-number-of-operations-to-make-array-xor-equal-to-k/
+
+from typing import *
+from leetgo_py import *
+
+from bisect import bisect_left, bisect_right
+from collections import Counter, defaultdict, deque
+from copy import deepcopy
+from functools import cache, cmp_to_key, lru_cache, reduce
+from heapq import heapify, heappop, heappush, heappushpop, heapreplace
+from itertools import accumulate, chain, count, pairwise, zip_longest
+from math import ceil, comb, floor, gcd, inf, isqrt, log2, perm, sqrt
+from operator import xor
+from string import ascii_lowercase
+from typing import List, Optional
+
+# @lc code=begin
+
+
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        N = 32
+        cnt = [0] * N
+        for x in nums:
+            for i in range(N):
+                if x >> i & 1:
+                    cnt[i] ^= 1
+
+        # print(cnt)
+        res = 0
+        for i in range(N):
+            res += (k >> i & 1) != (cnt[i] & 1)
+        return res
+
+
+# @lc code=end
+
+if __name__ == "__main__":
+    nums: List[int] = deserialize("List[int]", read_line())
+    k: int = deserialize("int", read_line())
+    ans = Solution().minOperations(nums, k)
+
+    print("\noutput:", serialize(ans))
