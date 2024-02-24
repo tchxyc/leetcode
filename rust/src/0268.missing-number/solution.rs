2ca74f2ff3b0ddfb0399d3b9f1c6b8939d53c41c
vthe <idashatao@gmail.com>
2024-02-24 14:58:34 +0800

    format

--- rust/src/0268.missing-number/solution.rs
+++ rust/src/0268.missing-number/solution.rs
@@ -11,19 +11,19 @@ struct Solution;
 
 impl Solution {
     pub fn missing_number(nums: Vec<i32>) -> i32 {
-		let n = nums.len() as i32;
-		let s = nums.iter().sum::<i32>();
-		
-		(n+1) * n / 2 - s
+        let n = nums.len() as i32;
+        let s = nums.iter().sum::<i32>();
+
+        (n + 1) * n / 2 - s
     }
 }
 
 // @lc code=end
 
 fn main() -> Result<()> {
-	let nums: Vec<i32> = deserialize(&read_line()?)?;
-	let ans: i32 = Solution::missing_number(nums).into();
+    let nums: Vec<i32> = deserialize(&read_line()?)?;
+    let ans: i32 = Solution::missing_number(nums).into();
 
-	println!("\noutput: {}", serialize(ans)?);
-	Ok(())
+    println!("\noutput: {}", serialize(ans)?);
+    Ok(())
 }
--- rust/src/0997.find-the-town-judge/solution.rs
+++ rust/src/0997.find-the-town-judge/solution.rs
@@ -11,31 +11,29 @@ struct Solution;
 
 impl Solution {
     pub fn find_judge(n: i32, trust: Vec<Vec<i32>>) -> i32 {
-		let n = n as usize;
-		let mut cnt = vec![0; n+1];
-		let mut can = vec![true;n+1];
-		for e in trust{
-			can[e[0] as usize] = false;
-			cnt[e[1] as usize] += 1;
-		}
-		for i in 1..=n{
-			if can[i] && cnt[i] == n-1{
-				return i as i32
-			}
-		}
-		-1
-
-        
+        let n = n as usize;
+        let mut cnt = vec![0; n + 1];
+        let mut can = vec![true; n + 1];
+        for e in trust {
+            can[e[0] as usize] = false;
+            cnt[e[1] as usize] += 1;
+        }
+        for i in 1..=n {
+            if can[i] && cnt[i] == n - 1 {
+                return i as i32;
+            }
+        }
+        -1
     }
 }
 
 // @lc code=end
 
 fn main() -> Result<()> {
-	let n: i32 = deserialize(&read_line()?)?;
-	let trust: Vec<Vec<i32>> = deserialize(&read_line()?)?;
-	let ans: i32 = Solution::find_judge(n, trust).into();
+    let n: i32 = deserialize(&read_line()?)?;
+    let trust: Vec<Vec<i32>> = deserialize(&read_line()?)?;
+    let ans: i32 = Solution::find_judge(n, trust).into();
 
-	println!("\noutput: {}", serialize(ans)?);
-	Ok(())
+    println!("\noutput: {}", serialize(ans)?);
+    Ok(())
 }
