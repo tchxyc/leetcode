bd5969a45ea5526bfbcdead73a39e81fd497fccb
vthe <idashatao@gmail.com>
2024-03-19 16:13:28 +0800

    Greedy

--- rust/src/0621.task-scheduler/solution.rs
+++ rust/src/0621.task-scheduler/solution.rs
@@ -15,46 +15,46 @@ use std::mem::swap;
 impl Solution {
     pub fn least_interval(tasks: Vec<char>, n: i32) -> i32 {
         let mut cnt = vec![0; 26];
-
+        let total = tasks.len();
         for ch in tasks {
             cnt[ch as usize - b'A' as usize] += 1;
         }
 
-        // let max = *cnt.iter().max().unwrap();
-        // let rest = cnt.iter().filter(|&x| *x == max).count() as i32;
+        let max = *cnt.iter().max().unwrap();
+        let rest = cnt.iter().filter(|&x| *x == max).count();
 
-        // (max - 1) * (n + 1) + rest
+        ((max - 1) * (n as usize + 1) + rest).max(total) as _
 
-        let mut q = BinaryHeap::new();
+        // let mut q = BinaryHeap::new();
 
-        for x in cnt {
-            if x != 0 {
-                q.push(x)
-            }
-        }
-		let n = n + 1; 
-        let mut res = 0;
-        while !q.is_empty() {
-            let mut tmp = vec![];
-            for i in 0..n {
-                if let Some(max) = q.pop() {
-                    res += 1;
-                    if max > 1 {
-                        tmp.push(max - 1)
-                    }
-                } else {
-                    if !tmp.is_empty() {
-                        res += n - i;
-                    }
-                    break;
-                }
-            }
+        // for x in cnt {
+        //     if x != 0 {
+        //         q.push(x)
+        //     }
+        // }
+        // let n = n + 1;
+        // let mut res = 0;
+        // while !q.is_empty() {
+        //     let mut tmp = vec![];
+        //     for i in 0..n {
+        //         if let Some(max) = q.pop() {
+        //             res += 1;
+        //             if max > 1 {
+        //                 tmp.push(max - 1)
+        //             }
+        //         } else {
+        //             if !tmp.is_empty() {
+        //                 res += n - i;
+        //             }
+        //             break;
+        //         }
+        //     }
 
-            for x in tmp {
-                q.push(x)
-            }
-        }
-        res
+        //     for x in tmp {
+        //         q.push(x)
+        //     }
+        // }
+        // res
     }
 }
 
