4d7f5afffe1a1ca38788f27f68394feea3c406bf
vthe <idashatao@gmail.com>
2024-03-25 15:46:10 +0800

    Hash

--- rust/src/0442.find-all-duplicates-in-an-array/solution.rs
+++ rust/src/0442.find-all-duplicates-in-an-array/solution.rs
@@ -13,17 +13,29 @@ use std::collections::*;
 use std::mem::swap;
 
 impl Solution {
-    pub fn find_duplicates(nums: Vec<i32>) -> Vec<i32> {
-        
+    pub fn find_duplicates(mut nums: Vec<i32>) -> Vec<i32> {
+        let mut res = vec![];
+        let n = nums.len();
+
+        for i in 0..n {
+            let x = nums[i].abs() as usize;
+            if nums[x - 1] < 0 {
+                res.push(x as _);
+            } else {
+                nums[x - 1] *= -1;
+            }
+        }
+
+        res
     }
 }
 
 // @lc code=end
 
 fn main() -> Result<()> {
-	let nums: Vec<i32> = deserialize(&read_line()?)?;
-	let ans: Vec<i32> = Solution::find_duplicates(nums).into();
+    let nums: Vec<i32> = deserialize(&read_line()?)?;
+    let ans: Vec<i32> = Solution::find_duplicates(nums).into();
 
-	println!("\noutput: {}", serialize(ans)?);
-	Ok(())
+    println!("\noutput: {}", serialize(ans)?);
+    Ok(())
 }
